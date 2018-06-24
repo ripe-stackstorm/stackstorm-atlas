80d96075217a52ba0c17bf3de4c71c65df881cf3
Jasper den Hertog <jdenhertog@ripe.net>
2018-06-24 15:46:03 +0100

    as events triggers

--- sensors/probes_disco_sensor.py
+++ sensors/probes_disco_sensor.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import datetime, timedelta
 
 from ripe.atlas.cousteau import (
     AtlasStream,
@@ -34,8 +35,6 @@ class ProbesDiscoSensor(Sensor):
         if r.status_code == 200:
             self._create_state_dicts(r.json()["probes"])
             # self._logger.info(self._create_probes_dict(r.json()["probes"]))
-            self._logger.info(self._ases_v4_state[3333])
-            self._logger.info(self._ases_v6_state[3333])
 
     def _create_state_dicts(self, probes):
         """
@@ -71,7 +70,67 @@ class ProbesDiscoSensor(Sensor):
             elif p[2] and p[2] != 0:
                 self._ases_v6_state[p[1]] = {status: set([p[0]])}
 
+        for asn_v4 in self._ases_v4_state:
+            # self._logger.info(asn_v4)
+            # self._logger.info(self._ases_v4_state)
+            self._update_vstate(self._ases_v4_state[asn_v4])
+
+        for asn_v6 in self._ases_v6_state:
+            self._update_vstate(self._ases_v6_state[asn_v6])
+
         # return {p[0]: {"prb_id": p[0], "asn_v4": p[1], "asn_v6": p[2], "status": STATUS_MAP[p[12]]} for p in probes}
+
+    def _update_vstate(self, vstate, prb_id=None, event=None):
+        def _update_disco_list(prb_id, event):
+            if event == "connect":
+                vstate.setdefault(
+                    "Disconnected", set()).discard(prb_id)
+                vstate.setdefault(
+                    "Connected", set()).add(prb_id)
+                try:
+                    del vstate.get("LastDisconnected", {})[prb_id]
+                except KeyError:
+                    pass
+                self._logger.info(vstate.get("LastDisconnected"))
+
+                for p in vstate.get("LastDisconnected", {}):
+                    if vstate["LastDisconnected"][p] < datetime.now() - timedelta(minutes=30):
+                        try:
+                            del vstate.get("LastDisconnected", {})[p]
+                        except KeyError:
+                            pass
+
+            if event == "disconnect":
+                vstate.setdefault(
+                    "Connected", set()).discard(prb_id)
+                vstate.setdefault(
+                    "Disconnected", set()).add(prb_id)
+
+                # add the prb_id to the disconnected sliding window
+                vstate.setdefault("LastDisconnected", {}).update(
+                    {prb_id: datetime.now()})
+                self._logger.info(vstate["LastDisconnected"])
+
+                # check for late disconnected and delete those from the sliding window
+                for p in vstate["LastDisconnected"]:
+                    if vstate["LastDisconnected"][p] < datetime.now() - timedelta(minutes=30):
+                        try:
+                            del vstate.get("LastDisconnected", {})[p]
+                        except KeyError:
+                            pass
+
+        if prb_id:
+            _update_disco_list(prb_id, event)
+
+        perc = 100.0 * float(len(vstate.get("Connected", []))) / float(
+            len(vstate.get("Connected", [])) +
+            len(vstate.get("Disconnected", []))
+        )
+
+        vstate["connection_percentage"] = perc
+        # self._logger.info('new perc: {}'.format(perc))
+
+        return perc
 
     def _update_probe_status(self, probe):
         """
@@ -102,38 +161,30 @@ class ProbesDiscoSensor(Sensor):
         p_state = self._probes_state.get(probe["prb_id"])
         event = probe["event"]
         prb_id = probe["prb_id"]
-        probe_p = probe["probe"]
         asn_v4 = probe["probe"]["asn_v4"]
         asn_v6 = probe["probe"]["asn_v6"]
-        if event == "disconnect" and p_state["status"] == "Connected":
+        if event == "disconnect":  # and p_state["status"] == "Connected":
             p_state["status"] = "Disconnected"
             if asn_v4:
-                self._ases_v4_state[asn_v4].setdefault(
-                    "Connected", set()).remove(prb_id)
-                self._ases_v4_state[asn_v4].setdefault(
-                    "Disconnected", set()).add(prb_id)
+                self._update_vstate(self._ases_v4_state[asn_v4], prb_id, event)
             if asn_v6:
-                self._ases_v6_state["asn_v6"].setdefault(
-                    "Connected", set()).remove(prb_id)
-                self._ases_v6_state["asn_v6"].setdefault(
-                    "Disconnected", set()).add(prb_id)
-        elif event == "connect" and p_state["status"] == "Disconnected":
+                self._update_vstate(self._ases_v6_state[asn_v6], prb_id, event)
+        elif event == "connect":  # and p_state["status"] == "Disconnected":
             p_state["status"] = "Connected"
             if asn_v4:
-                self._ases_v4_state["asn_v4"].setdefault(
-                    "Disconnected", set()).remove(prb_id)
-                self._ases_v4_state["asn_v4"].setdefault(
-                    "Connected", set()).add(prb_id)
+                self._update_vstate(self._ases_v4_state[asn_v4], prb_id, event)
             if asn_v6:
-                self._ases_v6_state["asn_v6"].setdefault(
-                    "Disconnected", set()).remove(prb_id)
-                self._ases_v6_state["asn_v6"].setdefault(
-                    "Connected", set()).add(prb_id)
+                self._update_vstate(self._ases_v6_state[asn_v6], prb_id, event)
+        else:
+            self._logger.info("probe {p} in wrong state".format(p=prb_id))
 
+        if (event == "disconnect" and p_state["status"] == "Disconnected") or (
+            event == "connect" and p_state["status"] == "Connected"
+        ):
+            self._logger.info("NO STATE CHANGE for probe {}".format(prb_id))
         self._logger.info(p_state)
         self._logger.info(probe["probe"])
-        self._logger.info(self._ases_v4_state.get(probe_p.get("asn_v4")))
-        self._logger.info(self._ases_v6_state.get(probe_p.get("asn_v6")))
+
         self._probes_state[probe["prb_id"]] = p_state
 
     def on_result_response(self, *args):
@@ -143,10 +194,22 @@ class ProbesDiscoSensor(Sensor):
         probe_update = args[0]
         prb_id = probe_update['prb_id']
         event = probe_update['event']
-        self._logger.info("Received a probe response")
-        self._logger.info(probe_update)
+        asn_v4 = probe_update['probe']["asn_v4"]
+        asn_v6 = probe_update['probe']["asn_v6"]
+        curr_asn_v4_conn = self._ases_v4_state.get(
+            asn_v4, {}).get('connection_percentage')
+        curr_asn_v6_conn = self._ases_v6_state.get(
+            asn_v6, {}).get('connection_percentage')
+        self._logger.info("Received a probe update for probe {prb_id} (asn v4:{asn_v4}, v6: {asn_v6}), event: \"{event}\"".format(
+            prb_id=prb_id, event=event, asn_v4=asn_v4, asn_v6=asn_v6))
+        # self._logger.info(probe_update)
         self._update_probe_status(probe_update)
+        new_asn_v4_conn = self._ases_v4_state.get(
+            asn_v4, {}).get('connection_percentage')
+        new_asn_v6_conn = self._ases_v6_state.get(
+            asn_v6, {}).get('connection_percentage')
 
+        # Evaluate single probe disco
         if event == "connect" or event == 'disconnect':
             trigger = 'atlas.probes_disco'
             payload = self._probes_state[prb_id]
@@ -155,6 +218,105 @@ class ProbesDiscoSensor(Sensor):
 
         self.sensor_service.dispatch(
             trigger=trigger, payload=payload, trace_tag=trace_tag)
+
+        # Evaluate ASes disco
+        if event == "disconnect":
+            trigger = 'atlas.probes_disco'
+
+            # AS presence goes below 50 percent
+            if curr_asn_v4_conn >= 50.0:
+                if new_asn_v4_conn < 50.0:
+                    self._logger.info('AWAS! asn_v4 {asn_v4} less than 50 percent connected!'.format(
+                        asn_v4=asn_v4))
+                    self.sensor_service.dispatch(
+                        trigger=trigger,
+                        payload={event: 'AWAS! asn_v4 {asn_v4} less than 50 percent connected!'.format(
+                            asn_v4=asn_v4)},
+                        trace_tag="{asn_v4}-lessthan-{timestamp}".format(
+                            asn_v4=asn_v4, timestamp=probe_update['timestamp'])
+                    )
+                elif new_asn_v4_conn - curr_asn_v4_conn <= 19.0:
+                    self._logger.info(
+                        'NO! asn_v4 {asn_v4} going down significantly'.format(asn_v4=asn_v4))
+                    self.sensor_service.dispatch(
+                        trigger=trigger, payload={event: 'NO! asn_v4 {asn_v4} going down significantly'.format(asn_v4=asn_v4)}, trace_tag="{asn_v4}-down-{timestamp}".format(
+                            asn_v4=asn_v4, timestamp=probe_update['timestamp']
+                        )
+                    )
+                if curr_asn_v6_conn >= 50.0 and new_asn_v6_conn < 50.0:
+                    self._logger.info('AWAS! asn_v6 {asn_v6} less than 50 percent connected!'.format(
+                        asn_v6=asn_v6))
+                    self.sensor_service.dispatch(
+                        trigger=trigger,
+                        payload={event: 'AWAS! asn_v6 {asn_v6} less than 50 percent connected!'.format(
+                            asn_v6=asn_v6)},
+                        trace_tag="{asn_v6}-lessthan-{timestamp}".format(
+                            asn_v4=asn_v4, timestamp=probe_update['timestamp']
+                        )
+                    )
+
+                elif new_asn_v6_conn and curr_asn_v6_conn and new_asn_v6_conn - curr_asn_v6_conn <= 19.0:
+                    self._logger.info(
+                        'NO! asn_v6 {asn_v6} going down significantly'.format(asn_v6=asn_v6))
+                    self.sensor_service.dispatch(
+                        trigger=trigger,
+                        payload={event: 'NO! asn_v6 {asn_v6} going down significantly'.format(
+                            asn_v6=asn_v6)},
+                        trace_tag="{asn_v6}-down-{timestamp}"
+                    )
+
+            # no AS presence
+            if curr_asn_v4_conn > 1.0 and new_asn_v4_conn < 1.0:
+                self._logger.info('{asn_v4} completely offline'.format(
+                    asn_v4=asn_v4))
+                self.sensor_service.dispatch(
+                    trigger=trigger,
+                    payload={event: '{asn_v4} completely offline'.format(
+                        asn_v4=asn_v4)},
+                    trace_tag="{asn_v4}-offline-{timestamp}".format(
+                        asn_v4=asn_v4, timestamp=probe_update['timestamp'])
+                )
+            if curr_asn_v6_conn > 1.0 and new_asn_v6_conn < 1.0:
+                self._logger.info('{asn_v6} completely offline'.format(
+                    asn_v6=asn_v6))
+                self.sensor_service.dispatch(
+                    trigger=trigger,
+                    payload={event: '{asn_v6} completely offline'.format(
+                        asn_v6=asn_v6)},
+                    trace_tag="{asn_v6}-offline-{timestamp}".format(
+                        asn_v6=asn_v6, timestamp=probe_update['timestamp'])
+                )
+
+            # AS picks up
+            if new_asn_v4_conn and curr_asn_v4_conn and new_asn_v4_conn - curr_asn_v4_conn > 19.0:
+                self._logger.info(
+                    'YEAH! {asn_v4} seeing significant uptake in online probes'.format(asn_v4=asn_v4))
+                self.sensor_service.dispatch(
+                    trigger=trigger,
+                    payload={event: 'YEAH! {asn_v4} seeing significant uptake in online probes'.format(
+                        asn_v4=asn_v4)},
+                    trace_tag="{asn_v4}-uptake-{timestamp}".format(
+                        asn_v4=asn_v4, timestamp=probe_update['timestamp'])
+                )
+            if new_asn_v6_conn and curr_asn_v6_conn and new_asn_v6_conn - curr_asn_v6_conn > 19.0:
+                self._logger.info(
+                    'YEAH! {asn_v6} seeing significant uptake in online probes'.format(asn_v6=asn_v6))
+                self.sensor_service.dispatch(
+                    trigger=trigger,
+                    payload={event: 'YEAH! {asn_v6} seeing significant uptake in online probes'.format(
+                        asn_v6=asn_v6)},
+                    trace_tag="{asn_v6}-uptake-{timestamp}".format(
+                        asn_v6=asn_v6, timestamp=probe_update['timestamp'])
+                )
+
+        self._logger.info('connection percentage for asn_v4 {asn_v4} {old} -> {new}'.format(
+            asn_v4=asn_v4, old=curr_asn_v4_conn, new=new_asn_v4_conn))
+        self._logger.info('connection percentage for asn_v6 {asn_v6} {old} -> {new}'.format(
+            asn_v6=asn_v6, old=curr_asn_v6_conn, new=new_asn_v6_conn))
+        self._logger.info('disconnect bin asn v4: {}'.format(
+            self._ases_v4_state.get(asn_v4, {}).get('LastDisconnected')))
+        self._logger.info('disconnect bin asn v6: {}'.format(
+            self._ases_v6_state.get(asn_v6, {}).get('LastDisconnected')))
 
     def run(self):
         stream_parameters = {"enrichProbes": True}
